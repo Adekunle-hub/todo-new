e8eed990108a8d42a141e170bde652f920183575
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-05-06 14:19:13 +0100

    third committ

--- server/index.js
+++ server/index.js
@@ -6,23 +6,37 @@ import todoRouter from "./routes/todoRoute.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://todo-new-alpha.vercel.app"
+];
 
 app.use(
   cors({
-    origin: "http://localhost:5173,https://todo-new-alpha.vercel.app",
+    origin: function (origin, callback) {
+   
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error(`CORS blocked: ${origin}`));
+    },
     credentials: true,
-  }),
+  })
 );
+
 app.use(express.json());
 
 app.use("/api/todos", todoRouter);
+
 connectDatabase()
   .then(() => {
-    console.log(" Database connected successfully");
+    console.log("Database connected successfully");
   })
   .catch((err) => {
-    console.error(" Database connection failed:", err);
+    console.error("Database connection failed:", err);
     process.exit(1);
   });
 
