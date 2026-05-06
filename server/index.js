6abde576ecce18971f9dca6eb10d87b822a546f5
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-05-06 14:12:49 +0100

    second committ

--- server/index.js
+++ server/index.js
@@ -8,14 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-
-
-app.use(cors({
-  origin: "http://localhost:5173,https://todo-new-alpha.vercel.app/", 
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173,https://todo-new-alpha.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
-
 
 app.use("/api/todos", todoRouter);
 connectDatabase()
@@ -26,10 +25,6 @@ connectDatabase()
     console.error(" Database connection failed:", err);
     process.exit(1);
   });
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
